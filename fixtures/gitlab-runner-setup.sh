af8c650d56c2141aff97e92a7453c0036ada5399
Vinicius Oyama <vinicius.oyama@gmail.com>
2018-09-05 11:44:06 -0300

    Added .sh script for machine setup

---
+++ fixtures/gitlab-runner-setup.sh
@@ -0,0 +1,19 @@
+sudo yum update -y
+sudo yum install -y docker
+sudo service docker start
+sudo usermod -a -G docker ec2-user
+
+sudo curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-$(uname -s)-$(uname -m) -o /usr/bin/docker-compose
+sudo chmod +x /usr/bin/docker-compose
+
+# Runner
+curl -L https://packages.gitlab.com/install/repositories/runner/gitlab-runner/script.rpm.sh | sudo bash
+sudo yum install -y gitlab-runner
+
+sudo mkdir -p /builds
+sudo mkdir -p /runners
+sudo mkdir -p /runners/cache
+sudo chmod -R 777 /builds
+sudo chmod -R 777 /runners
+
+sudo gitlab-runner register --registration-token TOKEN --url https://git.dev.codus.com.br/ci --builds-dir /builds --docker-volumes /runners/cache:/cache  --executor docker  --docker-shm-size 4000000000 --docker-privileged true
